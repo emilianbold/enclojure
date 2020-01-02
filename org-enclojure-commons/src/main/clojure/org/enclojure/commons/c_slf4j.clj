9002334ca977d7641b4f2ebfdc5406dc86f43d5a
Emilian Bold <emilian.bold@gmail.com>
2020-01-02 11:28:50 +0200

    Compiles for RELEASE112 with Java 11. Includes beans-binding (since NetBeans deleted it), gets rid of slf4j and bumps clojure-maven-plugin / nbm-maven-plugin versions.

--- org-enclojure-commons/src/main/clojure/org/enclojure/commons/c_slf4j.clj
+++ org-enclojure-commons/src/main/clojure/org/enclojure/commons/c_slf4j.clj
@@ -16,13 +16,12 @@ These macros attempt to put as little additional scafolding as possible on the
 sslf4j lib.  It all cases the calls should be the same as direct calls to the
 slf4j lib convenience functions."}
  org.enclojure.commons.c-slf4j
- (:import (org.slf4j Logger LoggerFactory))
  )
 
 (defmacro ensure-logger []
    (let [nsym# (str (ns-name *ns*))]
-    `(~@(list 'defonce (with-meta '--logger-- {:private true :tag  'org.slf4j.Logger}))
-           (org.slf4j.LoggerFactory/getLogger ~nsym#))))
+    `(~@(list 'defonce (with-meta '--logger-- {:private true :tag  'java.util.logging.Logger}))
+           (java.util.logging.Logger/getLogger ~nsym#))))
 
 (defmacro log
   "macro to call log passing in a level.  Users should use the convenience macros:
@@ -31,32 +30,34 @@ NOTE: the def-logging-fn must be called in your file in order to use these macro
 It creates a logger using the namespace bound to *ns* at compile time."
   ([level msg]
     `(let [msg# ~msg]
-       (~@(list '. '--logger--) (~level #^String msg#))))
+       ;TODO: set level
+       (~@(list '. '--logger--) (log java.util.logging.Level/WARNING #^String msg#))))
   ([level fmt obj]    
     `(let [fmt# ~fmt obj# ~obj]
-        (~@(list '. '--logger--) (~level #^String fmt# obj#))))
+       ;TODO: set level
+        (~@(list '. '--logger--) (log java.util.logging.Level/WARNING #^String fmt# obj#))))
   ([level fmt obj1 obj2]
     `(let [fmt# ~fmt obj1# ~obj1 obj2# ~obj2]
-        (~@(list '. '--logger--) (~level #^String fmt# obj1# obj2#))))
+        (~@(list '. '--logger--) (log java.util.logging.Level/WARNING #^String fmt# (to-array [obj1# obj2#])))))
   ([level fmt obj1 obj2 objs]
     `(let [fmt# ~fmt 
            objs# (into-array java.lang.Object  [~obj1 ~obj2 ~@objs])]
         (~@(list '. '--logger--)
-            (~level  #^String ~fmt #^"[Ljava.lang.Object;" objs#)))))
+            (log java.util.logging.Level/WARNING #^String ~fmt #^"[Ljava.lang.Object;" objs#)))))
 
 (defmacro log-throwable
   ([level msg throwable]
     `(let [msg# ~msg
            throwable# ~throwable]
         (~@(list '. '--logger--)
-            (~level #^String msg# #^Throwable throwable#)))))
+            (log java.util.logging.Level/WARNING #^String msg# #^Throwable throwable#)))))
 
 (defmacro 
   ;#^{:private true}
   make-level-macros
   "Macro to define the convenience functions for this module.
 Users do not call this directly."
-  [level]
+  [level julLevel]
   (let [t# (symbol (str level "-throwable"))]
       `(do         
          (defmacro ~level
@@ -78,9 +79,9 @@ Users do not call this directly."
                  `(log-throwable ~s# ~msg# ~throwable#)))))))
 
 ; Define the convenience macros
-(make-level-macros debug)
-(make-level-macros error)
-(make-level-macros trace)
-(make-level-macros warn)
-(make-level-macros info)
+(make-level-macros debug java.util.logging.Level/FINE)
+(make-level-macros error java.util.logging.Level/SEVERE)
+(make-level-macros trace java.util.logging.Level/FINEST)
+(make-level-macros warn java.util.logging.Level/WARNING)
+(make-level-macros info java.util.logging.Level/INFO)
 
